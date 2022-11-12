d2fc63ea0fcfce8f79e15667d677e402636470a6
Juyie <ghkdwndl1004@ewhain.net>
2022-11-12 16:32:01 +0900

    레벨 추가, 번개 장애물 추가

---
+++ Sun the Light/Assets/Scripts/Level/Level1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level1 : Level
+{
+    private float[] timeArr = { 2f, 5f };
+    private int[] obstacleIndexArr = { 0, 6};
+    private float[] angleArr = { -15f, 15f};
+    private float[] speedArr = { 2f, 5f };
+
+    override public void SetLevel()
+    {
+        Debug.Log("Set Level, " + this.timeArr);
+        CreateObstacle createObstacle = GameObject.Find("ObstacleManager").GetComponent<CreateObstacle>();
+        createObstacle.timeArr = this.timeArr;
+        createObstacle.obstacleIndexArr = this.obstacleIndexArr;
+        createObstacle.angleArr = this.angleArr;
+        createObstacle.speedArr = this.speedArr;
+    }
+}
--- Sun the Light/Assets/Scripts/Obstacle/CreateObstacle.cs
+++ Sun the Light/Assets/Scripts/Obstacle/CreateObstacle.cs
@@ -16,7 +16,12 @@ public class CreateObstacle : MonoBehaviour
     private float angle;
     private float speed;
     private float direction;
-    private float[] directionArr = { -1, 1};
+
+    public float[] timeArr;
+    public int[] obstacleIndexArr;
+    public float[] angleArr;
+    public float[] speedArr;
+    public float[] directionArr = { -1, 1};
 
     // Start is called before the first frame update
     void Start()
@@ -37,23 +42,41 @@ public class CreateObstacle : MonoBehaviour
     private void Create()
     {
         // 랜덤 값 설정
-        obstacleIndex = Random.Range(0, obstaclePrefabs.Length);
-        angle = Random.Range(-20f, 20f);
-        speed = Random.Range(2f, 6f);
-        direction = directionArr[Random.Range(0, 2)];
-
-        GameObject obstacle = Instantiate(obstaclePrefabs[obstacleIndex]);
-        if(direction == 1)
+        obstacleIndex = Random.Range(obstacleIndexArr[0], obstacleIndexArr[1]);
+        if (obstacleIndex == 6 || obstacleIndex == 7)
         {
-            obstacle.transform.position = createPoints[0].transform.position;
+            angle = 0;
+            speed = 0;
+            direction = 0;
         }
         else
         {
-            obstacle.transform.position = createPoints[1].transform.position;
+            angle = Random.Range(angleArr[0], angleArr[1]);
+            speed = Random.Range(speedArr[0], speedArr[1]);
+            direction = directionArr[Random.Range(0, 2)];
+        }
+
+        GameObject obstacle = Instantiate(obstaclePrefabs[obstacleIndex]);
+
+        if (obstacleIndex == 6 || obstacleIndex == 7)
+        {
+            float randomX = Random.Range(-7.5f, 7.5f);
+            obstacle.transform.position = new Vector3 (randomX, 0, 0);
+        }
+        else
+        {
+            if (direction == 1)
+            {
+                obstacle.transform.position = createPoints[0].transform.position;
+            }
+            else
+            {
+                obstacle.transform.position = createPoints[1].transform.position;
+            }
         }
         obstacle.GetComponent<MoveObstacle>().SetOption(angle, speed, direction);
 
-        time = Random.Range(1f, 4f);
+        time = Random.Range(timeArr[0], timeArr[1]);
         canCreate = true;
     }
 }
