8878a7dac5df1ad88cfad2b7dd225105f869f0f6
Juyie <ghkdwndl1004@ewhain.net>
2022-11-12 11:41:05 +0900

    장애물 랜덤 생성 구현

---
+++ Sun the Light/Assets/Scripts/Obstacle/CreateObstacle.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateObstacle : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] obstaclePrefabs;
+
+    [SerializeField]
+    private GameObject[] createPoints;
+
+    private bool canCreate = true;
+    private float time;
+    private int obstacleIndex;
+    private float angle;
+    private float speed;
+    private float direction;
+    private float[] directionArr = { -1, 1};
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canCreate)
+        {
+            Invoke("Create", time);
+            canCreate = false;
+        }
+    }
+
+    private void Create()
+    {
+        // 랜덤 값 설정
+        obstacleIndex = Random.Range(0, obstaclePrefabs.Length);
+        angle = Random.Range(-20f, 20f);
+        speed = Random.Range(2f, 6f);
+        direction = directionArr[Random.Range(0, 2)];
+
+        GameObject obstacle = Instantiate(obstaclePrefabs[obstacleIndex]);
+        if(direction == 1)
+        {
+            obstacle.transform.position = createPoints[0].transform.position;
+        }
+        else
+        {
+            obstacle.transform.position = createPoints[1].transform.position;
+        }
+        obstacle.GetComponent<MoveObstacle>().SetOption(angle, speed, direction);
+
+        time = Random.Range(1f, 6f);
+        canCreate = true;
+    }
+}
--- Sun the Light/Assets/Scripts/Obstacle/MoveObstacle.cs
+++ Sun the Light/Assets/Scripts/Obstacle/MoveObstacle.cs
@@ -13,6 +13,13 @@ public class MoveObstacle : MonoBehaviour
     [SerializeField]
     private float direction;
 
+    public void SetOption(float angle, float speed, float direction)
+    {
+        this.angle = angle;
+        this.speed = speed;
+        this.direction = direction;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -30,5 +37,10 @@ public class MoveObstacle : MonoBehaviour
     void Update()
     {
         gameObject.transform.Translate(-speed * Time.deltaTime, 0, 0);
+
+        if(gameObject.transform.position.x > 20 || gameObject.transform.position.x < -20)
+        {
+            Destroy(gameObject);
+        }
     }
 }
