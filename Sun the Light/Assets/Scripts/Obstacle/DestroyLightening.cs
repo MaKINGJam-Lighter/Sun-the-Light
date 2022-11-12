1ab82fea093153341fccd5f296cfd78c9d82b58a
Juyie <ghkdwndl1004@ewhain.net>
2022-11-12 17:19:21 +0900

    장애물 효과음 추가

--- Sun the Light/Assets/Scripts/Obstacle/DestroyLightening.cs
+++ Sun the Light/Assets/Scripts/Obstacle/DestroyLightening.cs
@@ -7,7 +7,7 @@ public class DestroyLightening : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Invoke("Destroy", 1.5f);
+        Invoke("Destroy", 5.5f);
     }
 
     // Update is called once per frame
