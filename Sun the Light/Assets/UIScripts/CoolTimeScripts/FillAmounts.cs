29a72c9ab77552acc3945ec0c53ed076fccc34a8
YujinJeon888888 <117812553+YujinJeon888888@users.noreply.github.com>
2022-11-12 17:06:43 +0900

    UI
    스킬 쿨타임-쿨타임 도는 동안 못 누르게: 됐는데 쿨 이미지가 안보임
    
    점수-maxScore아직 x
    
    체력바-o
    
    배경- o
    
    재시작 버튼-x
    
    일시정지버튼-o(enter버튼)

---
+++ Sun the Light/Assets/UIScripts/CoolTimeScripts/FillAmounts.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FillAmounts : MonoBehaviour
+{
+    public Image img_Skill;
+
+    void Update()
+    {
+        //스킬버튼누르면
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            //인자: 쿨타임 초
+            StartCoroutine(CoolTime(13f));
+        }    
+    }
+
+    IEnumerator CoolTime(float cool)
+    {
+        print("쿨타임 코루틴 실행");
+
+        while (cool >1.0f)
+        {
+            //이거하는동안 클릭 못하게
+
+            cool -= Time.deltaTime;
+            img_Skill.fillAmount = (1.0f / cool);
+            yield return new WaitForFixedUpdate();
+        }
+
+        print("쿨타임 코루틴 완료");
+    }
+}
