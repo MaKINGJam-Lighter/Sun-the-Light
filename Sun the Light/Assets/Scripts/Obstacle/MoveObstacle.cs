0ee3a3424f2bbb8f547bbb2fc78e28c21ae9bede
Juyie <ghkdwndl1004@ewhain.net>
2022-11-12 11:11:49 +0900

    장애물 prefab 생성 및 이동 구현

---
+++ Sun the Light/Assets/Scripts/Obstacle/MoveObstacle.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObstacle : MonoBehaviour
+{
+    [SerializeField]
+    private float angle;
+
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private float direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (direction == 1)
+        {
+            gameObject.transform.Rotate(0, 180, angle);
+        }
+        else
+        {
+            gameObject.transform.Rotate(0, 0, angle);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Translate(-speed * Time.deltaTime, 0, 0);
+    }
+}
