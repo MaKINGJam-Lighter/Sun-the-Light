5e96e1d99d2c52b073bb950d5b35fda5ef5a3b56
Gamddalki <hannahk01@naver.com>
2022-11-13 16:43:14 +0900

    Clear Animation

---
+++ Sun the Light/Assets/Scripts/Start/Deactivate1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deactivate1 : MonoBehaviour
+{
+    public TypeEffect tf;
+    public GameObject next;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (tf.done == true)
+            Invoke("deactive", 0.1f);
+    }
+
+    private void deactive()
+    {
+        next.SetActive(false);
+    }
+}
