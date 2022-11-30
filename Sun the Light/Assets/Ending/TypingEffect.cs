5e66f4be834beb84f23ebad79db6f9b0e139fae1
YujinJeon888888 <117812553+YujinJeon888888@users.noreply.github.com>
2022-11-30 17:58:24 +0900

    메인으로버튼, 스킵버튼 추가
    + 대사 끝나면 종이 사라지게

--- Sun the Light/Assets/Ending/TypingEffect.cs
+++ Sun the Light/Assets/Ending/TypingEffect.cs
@@ -12,6 +12,9 @@ public class TypingEffect : MonoBehaviour
     private string Msg = "";
     public GameObject next;
     public GameObject restartBtn;
+    public GameObject paper;
+    public GameObject toMain;
+
     //텍스트 가져오기
     //public GameObject gameOverMsg;
 
@@ -30,10 +33,15 @@ public class TypingEffect : MonoBehaviour
             tx.enabled = false;
             if (next)
                 next.SetActive(true);
-            else//타이핑 끝나면 리스타트버튼 활성화
+            else//타이핑 끝나면 리스타트버튼 활성화, paper비활성화
             {
+                //리스타트버튼 부르고
                 restartBtn.SetActive(true);
-                //gameOverMsg.SetActive(true);
+                //메인으로 버튼도 부르고
+                toMain.SetActive(true);
+                //이 오브젝트는 비활성화 바이..
+                paper.SetActive(false);
+
             }
         }
     }
