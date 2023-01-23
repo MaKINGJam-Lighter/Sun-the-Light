788d3b087da705be83570c851c40fdafc86c29c5
Gamddalki <hannahk01@naver.com>
2023-01-23 20:16:08 +0900

    hp 코드 주석처리

--- Sun the Light/Assets/wine.cs
+++ Sun the Light/Assets/wine.cs
@@ -22,6 +22,15 @@ public class wine : MonoBehaviour
         {
             //IncreaseHP();
             Destroy(gameObject);
+            //IncreaseHP
+            IncreaseHP();
+
         }
+    }
+
+    private void IncreaseHP()
+    {
+        //playerEffectAudioSource.Play();
+        //healthBar.value += hp;
     }
 }
