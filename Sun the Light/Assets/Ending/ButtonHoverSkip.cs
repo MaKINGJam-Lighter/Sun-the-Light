e05a54b52817992b55ad9d8558a4348d7f63d673
YujinJeon888888 <117812553+YujinJeon888888@users.noreply.github.com>
2023-02-15 02:45:53 +0900

    버튼오버_시작애니

---
+++ Sun the Light/Assets/Ending/ButtonHoverSkip.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ButtonHoverSkip : MonoBehaviour
+{
+    public GameObject skip;
+
+    //마우스포인터가 위로 올라오면
+    public void IfOnMousePointer()
+    {
+        Text btnText = skip.GetComponentInChildren<Text>();
+        btnText.fontSize = 60;
+    }
+
+    //마우스포인터가 밖으로 나가면
+    public void IfOutMousePointer()
+    {
+        Text btnText = skip.GetComponentInChildren<Text>();
+        btnText.fontSize = 50;
+    }
+}
