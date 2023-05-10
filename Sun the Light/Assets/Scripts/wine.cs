ca45bb1673551ad7a5a8255abeeb0d54e2e8722d
YujinJeon888888 <117812553+YujinJeon888888@users.noreply.github.com>
2023-05-10 19:33:33 +0900

    와인 먹어지게

--- Sun the Light/Assets/Scripts/wine.cs
+++ Sun the Light/Assets/Scripts/wine.cs
@@ -1,36 +1,47 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 
-//public class wine : MonoBehaviour
-//{
+public class wine : MonoBehaviour
+{
+    public Slider healthBar;
+    public GameObject parent;
+
+    [SerializeField]
+    private float hp;
+
     // Start is called before the first frame update
-//    void Start()
-//    {
-        
-//    }
+    void Start()
+    {
+        healthBar = GameObject.FindWithTag("HP").GetComponent<Slider>();
+    }
 
     // Update is called once per frame
-//    void Update()
-//    {
+    void Update()
+    {
+        if (gameObject.transform.position.x > 20 || gameObject.transform.position.x < -20)
+        {
+            Destroy(gameObject);
+        }
+    }
 
-//    }
-
-//    private void OnTriggerEnter2D(Collider2D collision)
-//    {
-//        if (collision.tag == "Player")
-//        {
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
             //와인 삭제
-//            Destroy(gameObject);
+            Destroy(gameObject);
             //IncreaseHP
-//            IncreaseHP();
+            IncreaseHP();
 
-//        }
-//    }
+        }
+    }
 
-//    private void IncreaseHP()
-//    {
+    private void IncreaseHP()
+    {
         //playerEffectAudioSource.Play();
-//        healthBar.value += hp;
-//    }
-//}
+        healthBar.value += hp;
+        Debug.Log("hp 20 증가");
+    }
+}
